68c1d69b1e3fb16e2a6f195ac386d3610e44690d
spoturno <spoturno16@gmail.com>
2021-10-10 12:56:12 -0300

    arreglo eigd, Z incomplento

--- eigd.m
+++ eigd.m
@@ -1,13 +1,7 @@
 
 function [v,d] = eigd (x)
-  [v,d]   = eig (x);
-  
-  %flip lef to right
-  v = fliplr(v);
-  
-  %d = flipud(fliplr(d));
-  d = diag(flipud(diag(d)));
-  % or use:  d = diag(flipud(diag(d)))
-                                  % ...whichever is faster 
-                                   
+  [v,d] = eig(x);
+  [D,I] = sort(diag(d));
+  d = diag(flipud(D));
+  v = v(:, flipud(I));
 end
