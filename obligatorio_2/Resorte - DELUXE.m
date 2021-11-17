300bbb67f1a436c797be92849129e9bc5e8bb596
guidoooo <90530683+guidoooo@users.noreply.github.com>
2021-11-16 21:25:58 -0300

    cambios de graficas
    primeras 3 : 50T
    cuarta: primeros 2T
    tercera: ulltimso 2T

--- obligatorio_2/Resorte - DELUXE.m
+++ obligatorio_2/Resorte - DELUXE.m
@@ -11,8 +11,6 @@ rango = 0:Dt:tmax;
 %
 u=[u0];
 v=[v0];
-uf=[];
-vf=[];
 %
 for i = rango
   a0 = -s1*(1+s2*u0^2)*u0; %%%
@@ -29,30 +27,19 @@ for i = rango
   u3 = u0 + Dt*v0 + (1/6)*(Dt^2)*a0 + (1/3)*(Dt^2)*a1;
   v3 = v0 + (1/6)*Dt*a0 + (2/3)*Dt*a1 + (1/6)*Dt*a2;
 %
-  if (i<=2*T) 
    u=[u,u3]; 
    v=[v,v3];
-  endif  
-  if (i>=48*T) 
-   uf=[uf,u3]; 
-   vf=[vf,v3];
-  endif  
 % 
   u0=u3;
   v0=v3;
 endfor
-
 urk=u;
 vrk=v;
-urkf=uf;
-vrkf=vf;
 %%%%%%%%%%%%%%%%%% MET3
 u0 = 1.5;
 v0 = 0;
 u=[u0];
 v=[v0];
-uf=[];
-vf=[];
 %
 for i = rango
   a0 = -s1*(1+s2*u0^2)*u0;
@@ -68,29 +55,19 @@ for i = rango
   u3 = u0 + Dt*v0 + 1/6*Dt^2*(a0 + a1 + a2);
   v3 = v0 + 1/4*Dt*(a0 + 3*a2);
 %
-  if (i<=2*T) 
    u=[u,u3]; 
    v=[v,v3];
-  endif  
-  if (i>=48*T) 
-   uf=[uf,u3]; 
-   vf=[vf,v3];
-  endif  
 %
   u0=u3;
   v0=v3;
 endfor
 umet3=u;
 vmet3=v;
-umet3f=uf;
-vmet3f=vf;
 %%%%%%%%%%%%%%%%%% MET4
 u0 = 1.5;
 v0 = 0;
 u=[u0];
 v=[v0];
-uf=[];
-vf=[];
 %
 for i = rango
   a0 = -s1*(1+s2*u0^2)*u0;% EC. 1
@@ -110,22 +87,14 @@ for i = rango
   u4 = u0 + Dt*v0 + 1/6*Dt^2*(a0 + 2*a2); % EC 8.1
   v4 = v0 + 1/6*Dt*(a0 + 4*a2 + a3); % EC 8.2
 %
-  if (i<=2*T) 
    u=[u,u4]; 
    v=[v,v4];
-  endif  
-  if (i>=48*T) 
-   uf=[uf,u4]; 
-   vf=[vf,v4];
-  endif  
 %
   u0=u4;
   v0=v4;
 endfor
 umet4=u;
 vmet4=v;
-umet4f=uf;
-vmet4f=vf;
 %%%%%%%%%%%%%%%%%%  GRAFICOS
 subplot (2, 3, 1)
 plot(urk,vrk,"-r"); % RUNGE KUTTA-3
@@ -134,6 +103,6 @@ plot(umet3,vmet3,"-b"); % MET3
 subplot (2, 3, 3)
 plot(umet4,vmet4,"-g"); % MET4
 subplot (2, 3, 4)
-plot(urk,vrk,"-r",umet3,vmet3,"-b",umet4,vmet4,"-g"); % PRIMEROS 2T
+plot(urk(1:66),vrk(1:66),"-r",umet3(1:66),vmet3(1:66),"-b",umet4(1:66),vmet4(1:66),"-g"); % PRIMEROS 2T
 subplot (2, 3, 5)
-plot(urkf,vrkf,"-r",umet3f,vmet3f,"-b",umet4f,vmet4f,"-g"); % ULTIMOS 2T
+plot(urk((length(urk)-64):length(urk)),vrk((length(vrk)-64):length(vrk)),"-r",umet3((length(umet3)-64):length(umet3)),vmet3((length(vmet3)-64):length(vmet3)),"-b",umet4((length(umet4)-64):length(umet4)),vmet4((length(vmet4)-64):length(vmet4)),"-g"); % ULTIMOS 2T
